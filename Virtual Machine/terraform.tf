bc9d038ad801229c27a9fb2a821cbb35597f4122
Amandeep Singh <samandeep540@gmail.com>
2025-07-08 14:43:26 +0530

    added backend

--- Virtual Machine/terraform.tf
+++ Virtual Machine/terraform.tf
@@ -5,10 +5,25 @@ terraform {
       version = "4.27.0"
     }
   }
+
+ 
+  # Configuration options
+  backend "azurerm" {
+    subscription_id = "b2cc0685-8560-4e34-af78-899f07b85130"
+    resource_group_name   = "terraform-rg"
+    storage_account_name  = "terrastateus848623976"
+    container_name        = "dev"
+    key                   = "terraform.tfstate"
+
+}
 }
 
-provider "azurerm" {
+ provider "azurerm" {
     features {}
-    subscription_id = "34b6d612-0e83-4362-97a7-ae95e493643a"
-  # Configuration options
-}
+    tenant_id = "1f9a39c3-19a1-4b99-a7ef-42d8755c192c"
+    subscription_id = "b2cc0685-8560-4e34-af78-899f07b85130"
+    client_id = "3653b5df-ac94-48c2-9757-cce48e1dce68"
+    client_secret = "oog8Q~CZ7iRMOKt6mjxzp0EtomWM2IZ8TIlDlciK"
+    
+  
+  }
